6292975bfa27f0424310b90c639d83f0898dfb89
Andrey <a_car11@yahoo.com>
2016-12-16 22:43:29 -0500

    correct wordings in home template

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,15 +2,16 @@
 
 @section('content')
 
-<h1>Welcome to Tasks'n things to remember Management</h1>
-<p class="lead">This is simple list if things we must do and we just forget because of our busy
-  schedule or life style. Who has room in our brain to remember all these little things sometimes, right?
-  Our list can help, it will be color coded to show you how the dates are going as far as been on time,
-  or if a task is about to became due.
-  The reason for colors is to help you not to focus too much
-  on reading dates, but to instruct your subconscious mind that something is about to be due
-  and ready to be completed, we that it's something fun!
-  Enjoy scheduling your tasksn' things to do!</p>
+<h1>Welcome to Tasks'n Things to Remember Management</h1>
+ <p class="lead">This is a list of things we must do and we just forget because of our busy
+  schedule or lifestyle. Our brain is full to its capacity most of the time and to remember
+  all these little things, its just tough, right?
+  Our list can help, it will be color coded to show how the dates are progressing
+  as far as being on time, or if a task is about to be due.
+  The reason for the colors is to help you not to focus too much on reading just plain dates,
+  but to instruct your subconscious mind that something is about to be due
+  and ready to be completed, hoping that it's something fun!</p>
+ <p class="lead">Enjoy scheduling your Tasks'n Things!</p>
 <hr>
 
 @if(Auth::check())
